8f0d5a6ad0bced6669884409c4a550dcc9fcfa1d
James Huang <sh694@duke.edu>
2023-02-13 16:21:37 -0500

    01

---
+++ test.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World";
+
+  return 0;
+}
+tream >
+
+    using namespace std;
+
+int main() {
+  cout << "Hello World";
+
+  return 0;
+}
